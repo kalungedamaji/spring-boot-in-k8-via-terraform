a35b0b35eb4385d1c5ba69626fab2ad8974c0e5c
damaji.kalunge <damaji.kalunge@gamesys.co.uk>
2024-05-07 14:23:04 +0530

    Nginx as side card proxy

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -59,7 +59,7 @@ resource "kubernetes_replication_controller" "hello_world" {
                 container_port = 8080
                 }
             name  = "nginx-sidecar"
-            image = "dkalunge/nginx-sidecar_js_80:js"
+            image = "dkalunge/nginx-sidecar_js:js"
       }
     }
   }
