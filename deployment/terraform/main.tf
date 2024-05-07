fb9468bcb10a2e2326a7c47d730c26fcd2d93265
damaji.kalunge <damaji.kalunge@gamesys.co.uk>
2024-05-07 17:10:13 +0530

    Nginx as side card proxy

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -59,7 +59,7 @@ resource "kubernetes_replication_controller" "hello_world" {
                 container_port = 8080
                 }
             name  = "nginx-sidecar"
-            image = "dkalunge/nginx-sidecar-js-1:js"
+            image = "dkalunge/nginx-sidecar-js-8:js"
       }
     }
   }
--- nginx-sidecar/publish_message.js
+++ nginx-sidecar/publish_message.js
@@ -1,11 +1,17 @@
-
+var response_body_arr = new Array();
 function capture_resp_send_client_sqs(res, data, flags) {
-    console.log("Nginx Request object: " + JSON.stringify(res, null, 2));
-    var response_body_arr = new Array();
+    console.log("Inside script ");
+    if (flags.last) {
+        console.log("Nginx Request object: " + JSON.stringify(res, null, 2));
+        response_body_arr.push(data);
+        console.log("Response body: " + JSON.stringify(response_body_arr.join('')));
+        res.sendBuffer(data, flags);
+        console.log(" sending message to Queue")
+        response_body_arr.length = 0;
+    }
     response_body_arr.push(data);
-    console.log("Response body: "+JSON.stringify(response_body_arr.join('')));
     res.sendBuffer(data, flags);
-    console.log(" sending message to Queue")
+
 
 }
 export default { capture_resp_send_client_sqs };
